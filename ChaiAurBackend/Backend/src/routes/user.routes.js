ce8adbd380fcbe0018dba4721669ce60e1809052
Sachins0 <sachins.ssingh0@gmail.com>
2024-02-04 22:30:40 +0530

    login and logout controllers

--- ChaiAurBackend/Backend/src/routes/user.routes.js
+++ ChaiAurBackend/Backend/src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
 import { upload } from "../middelwares/multer.middlewares.js";
+import { verifyJWT } from "../middelwares/auth.middleware.js";
 
 const router=Router()
 
@@ -16,5 +17,10 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginUser)
+
+//secured rotes
+router.route("/logout").post(verifyJWT,logoutUser)
 
 export default router
