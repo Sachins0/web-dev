35a84c7b1610adbd3db6125d135e44ae2e776014
Sachins0 <sachins.ssingh0@gmail.com>
2024-02-05 11:10:26 +0530

    all bugs fixed before lec 16

--- ChaiAurBackend/Backend/src/routes/user.routes.js
+++ ChaiAurBackend/Backend/src/routes/user.routes.js
@@ -1,26 +1,32 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
-import { upload } from "../middelwares/multer.middlewares.js";
-import { verifyJWT } from "../middelwares/auth.middleware.js";
+import { 
+    loginUser, 
+    logoutUser, 
+    registerUser
+} from "../controllers/user.controller.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router=Router()
+
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
-        },
+            name: "avatar",
+            maxCount: 1
+        }, 
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
-    registerUser)
+    registerUser
+    )
 
 router.route("/login").post(loginUser)
 
-//secured rotes
-router.route("/logout").post(verifyJWT,logoutUser)
+//secured routes
+router.route("/logout").post(verifyJWT,  logoutUser)
 
 export default router
